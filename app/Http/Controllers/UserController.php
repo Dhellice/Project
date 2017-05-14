6efb14691be52a06fbeeb7ec85281d969ef01239
Margauw <margaux.dubezin@devinci.fr>
2017-05-14 16:16:43 +0200

    profil

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+
+        return view('users.index', ['users' => $users]);
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,10 @@ Route::get('/home', 'HomeController@index');
 Route::resource('/series', 'SerieController');
 Route::resource('/episodes', 'EpisodeController');
 Route::resource('/comments', 'CommentsController');
+Route::resource('/user', 'UserController');
+
 
 Route::post('/series/{serie}/comments', 'CommentsController@store');
-
-
 
 
 Route::get('/contact',

28bd8903ffa606c1ae5a7f5e6de83a0ef4d81552
Margauw <margaux.dubezin@devinci.fr>
2017-05-17 14:04:15 +0200

    Lien Forum Fabien est une merde

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,11 +55,11 @@
                             <li><a href="{{ route('login') }}">Connexion</a></li>
                             <li><a href="{{ route('register') }}">Inscription</a></li>
                             <li><a href="{{ route('series.index') }}">Séries</a></li>
-                            <li><a href="forums">Forum</a></li>
+                            <li><a href="{{ route('chatter.home') }}">Forum</a></li>
                         @else
                             <li><a href="{{ route('user.index') }}">Profil</a></li>
                             <li><a href="{{ route('series.index') }}">Séries</a></li>
-                            <li><a href="forums">Forum</a></li>
+                            <li><a href="{{ route('chatter.home') }} ">Forum</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>

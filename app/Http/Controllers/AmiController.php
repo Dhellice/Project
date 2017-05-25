037b8fb7af14b5e84c8c5c88aba0cf336caa63f7
Margauw <margaux.dubezin@devinci.fr>
2017-05-25 18:01:17 +0200

    Suppression Amis

--- app/Http/Controllers/AmiController.php
+++ app/Http/Controllers/AmiController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Like;
+use App\Serie;
 use App\User;
 use App\Ami;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Request;
 
 class AmiController extends Controller
 {
@@ -32,7 +36,7 @@ class AmiController extends Controller
     public function handleAmi($id)
     {
         $users = User::find($id);
-        $existing_ami = Ami::withTrashed()->whereAmiId($id)->whereUserId(Auth::id())->first();
+        $existing_ami = DB::table('amis')->whereAmiId($id)->whereUserId(Auth::id())->first();
 
         if (is_null($existing_ami)) {
             Ami::create([
@@ -44,5 +48,53 @@ class AmiController extends Controller
         } else {
             return redirect()->route('user.index');
         }
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+        $users = User::all();
+        $likeables = Like::all();
+        $series = Serie::all();
+        $ami = Ami::find($id);
+
+
+        if(!$ami) {
+            return redirect()->route('users.index');
+        }
+
+        return view('users.show', compact('users', 'likeables', 'series', 'ami'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Comments  $comments
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $ami = Ami::find($id);
+
+        if(!$ami) {
+            return redirect()->route('user.index');
+        }
+
+        return view('amis.edit', compact('ami'));
+    }
+
+
+    public function destroy($id)
+    {
+        $ami = Ami::find($id);
+
+        $ami->delete();
+
+        return redirect()->route('user.index')->with('success', 'Ami supprimé');
     }
 }

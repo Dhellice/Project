4980dc0650529f697460f0962af268146bf98a5f
Dhellice <louise.danicourt@hotmail.fr>
2017-05-25 19:49:30 +0200

    photo ami

--- resources/views/amis/index.blade.php
+++ resources/views/amis/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="panel-heading">Les series</div>
+            <div class="panel-heading"><h1>Ajoutez aux amis</h1></div>
             @if(session('success'))
                 <div class="alert alert-success">
                     {{session('success')}}
@@ -14,6 +14,7 @@
                 @if (Auth::user()->id != $user->id )
 
                 <div class="col-xs-5 col-md-4 block">
+                    <img src="/img/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius: 50%; margin-right:25px">
                     <h3 class="title">{{ $user->name }}</h3>
                     <br>
                     @if(Auth::check())
